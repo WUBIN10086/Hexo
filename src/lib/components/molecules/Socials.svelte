337999102a35e94db78d782866a1b6e3a04dcbb5
WUBIN10086 <wubin990225@gmail.com>
2024-06-05 04:08:54 +0900

    add

--- src/lib/components/molecules/Socials.svelte
+++ src/lib/components/molecules/Socials.svelte
@@ -5,11 +5,20 @@
 	import EmailIcon from '$lib/icons/socials/email.svelte';
 	import RssIcon from '$lib/icons/rss.svelte';
 	import MastodonIcon from '$lib/icons/socials/mastodon.svelte';
+	import { writable } from 'svelte/store';
+
+	let showEmail = writable(false);
+	let email = 'wubin_workmail@163.com';
+
+	function copyEmail() {
+		navigator.clipboard.writeText(email);
+	}
 </script>
 
 <div class="socials">
+
 	<a
-		href="#"
+		href="https://t.me/yourusername"
 		target="_blank"
 		rel="noopener noreferrer"
 		title="Say Hi on Telegram"
@@ -17,7 +26,7 @@
 		<TelegramIcon />
 	</a>
 	<a
-		href="#"
+		href="https://mastodon.social/@yourusername"
 		target="_blank"
 		rel="me noreferrer"
 		title="Say Hi on Mastodon"
@@ -25,7 +34,7 @@
 		<MastodonIcon />
 	</a>
 	<a
-		href="#"
+		href="https://github.com/wubin10086"
 		target="_blank"
 		rel="noopener noreferrer"
 		title="See my GitHub profile"
@@ -33,21 +42,23 @@
 		<GitHubIcon />
 	</a>
 	<a
-		href="#"
+		href="https://www.linkedin.com/in/yourusername/"
 		target="_blank"
 		rel="noopener noreferrer"
 		title="Connect on LinkedIn"
 	>
 		<LinkedInIcon />
 	</a>
-	<a
-		href="#"
-		target="_blank"
-		rel="noopener noreferrer"
-		title="Send an email"
-	>
-		<EmailIcon />
-	</a>
+	<div class="email-container" on:mouseenter={() => showEmail.set(true)} on:mouseleave={() => showEmail.set(false)}>
+		<a on:click={copyEmail} title="Send an email">
+			<EmailIcon/>
+		</a>
+		{#if $showEmail}
+			<div class="email-popup">
+				<p>{email}</p>
+			</div>
+		{/if}
+	</div>
 </div>
 
 <style lang="scss">
@@ -57,11 +68,14 @@
 		align-items: center;
 		gap: 20px;
 
-		a {
+		a, button {
 			transition: all 0.2s ease-in-out;
 			width: 24px;
 			color: var(--color--text);
 			fill: var(--color--text);
+			background: none;
+			border: none;
+			cursor: pointer;
 
 			&:hover {
 				color: var(--color--primary);
@@ -69,5 +83,49 @@
 				filter: drop-shadow(0px 0px 3px var(--color--primary));
 			}
 		}
+	}
+
+	.email-container {
+		display: flex;
+		align-items: center;
+		// gap: 20px;
+		a, button {
+			transition: all 0.2s ease-in-out;
+			width: 24px;
+			color: var(--color--text);
+			fill: var(--color--text);
+			background: none;
+			border: none;
+			cursor: pointer;
+
+			&:hover {
+				color: var(--color--primary);
+				fill: var(--color--primary);
+				filter: drop-shadow(0px 0px 3px var(--color--primary));
+			}
+		}
+	}
+
+	.email-popup {
+		position: relative;
+		bottom: 100%;
+		left: 20%;
+		transform: translateX(-20%);
+		background-color: #333;
+		color: #fff;
+		padding: 5px;
+		padding-top: 0;
+		padding-bottom: 0;
+		border-radius: 5px;
+		white-space: nowrap;
+		z-index: auto;
+		opacity: 0;
+		visibility: hidden;
+		transition: opacity 0.2s, visibility 0.2s;
+	}
+
+	.email-container:hover .email-popup {
+		opacity: 1;
+		visibility: visible;
 	}
 </style>
